ae3e01691c848b7dda669e19560452f4b1893579
Bill Wallace <wayfarer3130@gmail.com>
2023-01-11 08:59:15 -0500

    Add netlify config fix and remove a console log

--- platform/ui/src/contextProviders/ViewportGridProvider.tsx
+++ platform/ui/src/contextProviders/ViewportGridProvider.tsx
@@ -77,9 +77,8 @@ export function ViewportGridProvider({ children, service }) {
           displaySetOptions,
           viewportLabel: viewportLabels[viewportIndex],
         };
-        const ret = { ...state, ...{ viewports } };
-        console.log('State is', JSON.stringify(ret, null, 1));
-        return ret;
+
+        return { ...state, ...{ viewports } };
       }
       case 'SET_LAYOUT': {
         const {
